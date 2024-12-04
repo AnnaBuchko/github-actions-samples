1d1d9e370b6197932df15a996a5b27969d467dbc
AnnaBuchko <ann.buchko@gmail.com>
2024-12-04 15:25:53 +0200

    test

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_2") == True:
-	message = "Hello, Sasha!"
+	message = "Hello, Anna!"
 else:
 	message = "Hello, World!"
 
