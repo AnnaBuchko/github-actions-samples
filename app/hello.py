a7e03d469aff70e6440b2e99d4f5963519365d4a
AnnaBuchko <ann.buchko@gmail.com>
2024-12-07 11:44:43 +0200

    Deploy to GKE with ArgoCD

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_2") == True:
-	message = "Hello, Anna! \n"
+	message = "Hello, ArgoCD! \n"
 else:
 	message = "Hello, World! \n"
 
